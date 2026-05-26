07b369b608030af01098ff0b7d3520fe15877937
Rahul Satish Chandak <118782915+rahulschandak@users.noreply.github.com>
2026-05-25 21:04:59 -0400

    Quick Sort

---
+++ QuickSort.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+
+class QuickSort {
+
+    private static int[] quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pIndex = partition(arr, low, high);
+            quickSort(arr, low, pIndex - 1);
+            quickSort(arr, pIndex + 1, high);
+        }
+
+        return arr;
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[low];
+        int i = low;
+        int j = high;
+        while (i < j) {
+            while (arr[i] <= pivot && i <= high - 1) {
+                i++;
+            }
+
+            while (arr[j] > pivot && j >= low + 1) {
+                j--;
+            }
+
+            if (i < j)
+                swap(arr, i, j);
+        }
+        swap(arr, low, j);
+        return j;
+     }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 6, 2, 5, 7, 9, 1, 3};
+        System.out.println(Arrays.toString(quickSort(arr, 0, arr.length - 1)));
+    }
+}
